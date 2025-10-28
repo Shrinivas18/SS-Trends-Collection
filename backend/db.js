2cc2d592c2d98fa846fd4c3ddde97cebde665c24
Shrinivas <jshrinivas1996@gmail.com>
2025-10-28 23:55:00 +0530

    initiated postgres connection

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+pool
+  .connect()
+  .then(() => console.log("✅ Connected to PostgreSQL"))
+  .catch((err) => console.error("❌ Database connection error:", err));
+
+export default pool;
